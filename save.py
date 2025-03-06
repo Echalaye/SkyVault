ae81b981319cf910f702a1bc94d3e7307f128f0e
Echalaye <etienne.chalaye@gmail.com>
2025-03-06 10:06:36 +0100

    publisher mosquito

--- save.py
+++ save.py
@@ -5,13 +5,9 @@ import time
 import umqtt.simple as MQTT
 
 # Configuration du WiFi
-SSID = "A15 de Etienne"
-PASSWORD = "lustucrU"
-
-
-
-
-
+SSID = "iPhone de Nathoo"
+PASSWORD = "RERT2070"
+mqtt_topic = "data/humidty"
 
 # Connexion au WiFi
 def connect_wifi():
@@ -28,33 +24,23 @@ def connect_wifi():
     print("Adresse IP:", wlan.ifconfig()[0])
     
     
-    
-    
-    
-    
-
-mqtt = MQTT.MQTTClient("esp32", "192.168.1.217", 1883, user="esp32-pub", password="pub-pswd")
+mqtt = MQTT.MQTTClient("esp32", "skyvault.local", 1883, user="esp32-pub", password="pub-pswd")
 def connect_to_mqtt_broker():
     #print("Connecting to MQTT broker")
     try:
         mqtt.connect()
-        
+        print("I'm connected to the MQTT server Bitch!!!")
     except MQTT.MQTTException as e:
         print("An MQTT error occured while trying to connect to the MQTT broker")
         print("Note: Check your connection parameters")
         print("Context:", e)
-        M.reset()
+        mqtt.reset()
         
     except Exception as e:
         print("An unknown error occured while trying to connect to the MQTT broker")
         print("Note: the connection most probably timed out")
         print("Context:", e)
-        M.reset()
-
-
-
-
-
+        mqtt.reset()
 
 
 # Initialisation du capteur DHT11
@@ -65,14 +51,11 @@ def read_sensor():
         try:
             sensor.measure()
             humidity = sensor.humidity()
+            mqtt.publish(mqtt_topic, str(humidity).encode())
             print(f"Humidité : {humidity} %")
         except Exception as e:
             print("Erreur de lecture du capteur:", e)
-        time.sleep(1)
-
-
-
-
+        time.sleep(2)
 
 
 # Exécution
